089c499f56587bb55d56733622f1b3fb03dd8710
imcauley <mcauleyisaac@gmail.com>
2020-03-18 21:30:21 -0600

    added int functions

--- Lambda.hs
+++ Lambda.hs
@@ -1,16 +1,20 @@
-import Control.Monad.State
+-- import Control.Monad.State
 
 data Lam a = App (Lam a) (Lam a)
            | Abst a (Lam a)
            | Var a
-           | LNum Int
-           | LNumFunc (Int -> Int -> Int)
+           | Const Int
+           | Func (Int -> Int -> Int) (Lam a) (Lam a)
+
 
 data DeBru a = DBApp (DeBru a) (DeBru a)
              | DBAbst (DeBru a)
              | DBVar a
              | DBRef Int
-             deriving (Show)
+            --  | DBNum Int
+             | DBConst Int
+             | DBFunc (Int -> Int -> Int) (DeBru a) (DeBru a)
+            --  deriving (Show)
 
 data MacCode a = MacApp
              | Acc Int
@@ -18,7 +22,9 @@ data MacCode a = MacApp
              | Clo [(MacCode a)]
              | CloEnv [(MacCode a)] [(MacCode a)]
              | MacVar a
-             deriving (Show)
+             | MacConst Int
+             | MacFunc (Int -> Int -> Int)
+            --  deriving (Show)
 
 
 type ConvertState a = (Int)
@@ -31,6 +37,9 @@ type EvalState a = ([MacCode a], [MacCode a], [MacCode a])
 
 omega = App (Abst "x" (App (Var "x") (Var "x"))) (Abst "x" (App (Var "x") (Var "x")))
 
+
+addTest = (App (Abst "x" (Func (\x y -> x + y) (Var "x") (Const 1))) (Const 2))
+
 --------------------------------------------------
 -- Lambda to de Buijn Conversion
 --------------------------------------------------
@@ -42,6 +51,9 @@ lambdaToDeBru (Var x) s
     | isBound = (DBRef (findIndex x s))
     | otherwise = (DBVar x)
     where isBound = (any ((==) x) s)
+
+lambdaToDeBru (Func f n m) s = (DBFunc f (lambdaToDeBru n s) (lambdaToDeBru m s))
+lambdaToDeBru (Const c) s = (DBConst c)
 
 findIndex :: Eq a => a -> [a] -> Int
 findIndex x (s:ss)
@@ -58,6 +70,8 @@ deBruToMacCode (DBVar v) = [(MacVar v)]
 deBruToMacCode (DBRef n) = [(Acc n)]
 deBruToMacCode (DBApp t1 t2) = (deBruToMacCode t2) ++ (deBruToMacCode t1) ++ [(MacApp)]
 deBruToMacCode (DBAbst f) = [(Clo ((deBruToMacCode f) ++ [Ret]))]
+deBruToMacCode (DBFunc f n m) =  (deBruToMacCode m) ++ (deBruToMacCode n) ++ [(MacFunc f)]
+deBruToMacCode (DBConst c) = [(MacConst c)]
 
 
 --------------------------------------------------
@@ -70,7 +84,7 @@ evaluateTerm l = do
         deBru = lambdaToDeBru l []
         code  = (deBruToMacCode deBru)
         result = evaluate code
-    return result
+    putStrLn (macCodeToString result)
 
 
 evaluate :: [MacCode a] -> (MacCode a)
@@ -86,10 +100,18 @@ codeStep :: EvalState a -> EvalState a
 codeStep (c:cs, env, stack) = 
     case c of
         (MacVar v) -> (cs, env, [(MacVar v)]++stack)
+        (MacConst c) -> (cs, env, [(MacConst c)]++stack)
         (MacApp)   -> unpackApplication (cs, env, stack)
         (Clo c) -> (cs, env, [(CloEnv c env)] ++ stack)
         (Acc n) -> (cs, env, [(env !! n)] ++ stack)
         (Ret)   -> (cs, fst (upackReturn stack), snd (upackReturn stack))
+        -- (MacFunc f) -> (applyFunction f cs env stack)
+
+applyFunction f cs env ((MacConst n):(MacConst m):stack) = (cs, env, (MacConst (f n m)):stack)
+applyFunction f cs env ((MacConst n):(MacVar m):stack) = (cs, env, (MacConst (f n m)):stack)
+applyFunction f cs env ((MacVar n):(MacConst m):stack) = (cs, env, (MacConst (f n m)):stack)
+applyFunction f cs env ((MacVar n):(MacVar m):stack) = (cs, env, (MacConst (f n m)):stack)
+
 
 upackReturn :: [(MacCode a)] -> ([MacCode a], [MacCode a])
 upackReturn (v:(CloEnv e s):_) = (e,v:s)
@@ -97,6 +119,7 @@ upackReturn (v:(CloEnv e s):_) = (e,v:s)
 unpackApplication :: EvalState a -> EvalState a
 unpackApplication (cs, env, ((CloEnv cs' env'):v:rest)) = (cs', (v:env'), (CloEnv cs env):rest)
 
+macCodeToString (MacConst c) = show c
 
 lambdaToString (Var a) = show a
 lambdaToString (Abst a l) = "λ" ++ show a ++ "." ++ (lambdaToString l)
