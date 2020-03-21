1f31c206ef86d115641628f60178adc3410bcf05
imcauley <mcauleyisaac@gmail.com>
2020-03-21 15:59:22 -0600

    Extended to bools

--- Lambda.hs
+++ Lambda.hs
@@ -3,20 +3,28 @@
 data Lam a = App (Lam a) (Lam a)
            | Abst a (Lam a)
            | Var a
+
+            --Extended For Integers
            | Const Int
            | Func (Int -> Int -> Int) (Lam a) (Lam a)
+
            | LBool Bool
            | BFunc (Int -> Int -> Bool) (Lam a) (Lam a)
            | IfThenElse (Lam a) (Lam a) (Lam a)
-
-
 
 data DeBru a = DBApp (DeBru a) (DeBru a)
              | DBAbst (DeBru a)
              | DBVar a
              | DBRef Int
+
+             --Extended For Integers
              | DBConst Int
              | DBFunc (Int -> Int -> Int) (DeBru a) (DeBru a)
+
+             -- Extended for Bools
+             | DBBool Bool
+             | DBBFunc (Int -> Int -> Bool) (DeBru a) (DeBru a)
+             | DBIf (DeBru a) (DeBru a) (DeBru a)
 
 data MacCode a = MacApp
              | Acc Int
@@ -24,9 +32,13 @@ data MacCode a = MacApp
              | Clo [(MacCode a)]
              | CloEnv [(MacCode a)] [(MacCode a)]
              | MacVar a
+
              | MacConst Int
              | MacFunc (Int -> Int -> Int)
-            --  deriving (Show)
+
+             | MBool Bool
+             | MBFunc (Int -> Int -> Bool)
+             | MacIf [(MacCode a)] [(MacCode a)]
 
 
 type ConvertState a = (Int)
@@ -41,6 +53,12 @@ omega = App (Abst "x" (App (Var "x") (Var "x"))) (Abst "x" (App (Var "x") (Var "
 
 
 addOne = App (Abst "x" (Func (\x y -> x + y) (Var "x") (Const 1))) -- Const (n)
+chooseOne = App (Abst "x" (IfThenElse (Var "x") (Const 1) (Const 2))) (LBool False)
+
+-- fact f n = y (if (n == 0) (1) (n * (f (n - 1))))
+
+-- fact = App y (IfThenElse (\x y ))
+
 
 -- λf.(λa.f (λx.a a x)) (λa.f (λx.a a x))
 
@@ -58,8 +76,14 @@ lambdaToDeBru (Var x) s
     | otherwise = (DBVar x)
     where isBound = (any ((==) x) s)
 
-lambdaToDeBru (Func f n m) s = (DBFunc f (lambdaToDeBru n s) (lambdaToDeBru m s))
-lambdaToDeBru (Const c) s = (DBConst c)
+lambdaToDeBru (Func f n m) s    = (DBFunc f (lambdaToDeBru n s) (lambdaToDeBru m s))
+lambdaToDeBru (Const c) s       = (DBConst c)
+
+lambdaToDeBru (LBool b) s         = (DBBool b)
+lambdaToDeBru (BFunc f n m) s     = (DBBFunc f (lambdaToDeBru n s) (lambdaToDeBru m s))
+lambdaToDeBru (IfThenElse i n m) s = (DBIf (lambdaToDeBru i s) (lambdaToDeBru n s) (lambdaToDeBru m s))
+
+
 
 findIndex :: Eq a => a -> [a] -> Int
 findIndex x (s:ss)
@@ -76,8 +100,13 @@ deBruToMacCode (DBVar v) = [(MacVar v)]
 deBruToMacCode (DBRef n) = [(Acc n)]
 deBruToMacCode (DBApp t1 t2) = (deBruToMacCode t2) ++ (deBruToMacCode t1) ++ [(MacApp)]
 deBruToMacCode (DBAbst f) = [(Clo ((deBruToMacCode f) ++ [Ret]))]
+
 deBruToMacCode (DBFunc f n m) =  (deBruToMacCode m) ++ (deBruToMacCode n) ++ [(MacFunc f)]
 deBruToMacCode (DBConst c) = [(MacConst c)]
+
+deBruToMacCode (DBBool b) = [(MBool b)]
+deBruToMacCode (DBBFunc f n m) = (deBruToMacCode m) ++ (deBruToMacCode n) ++ [(MBFunc f)]
+deBruToMacCode (DBIf i n m) = (deBruToMacCode i) ++ [MacIf ((deBruToMacCode n) ++ [Ret]) ((deBruToMacCode m) ++ [Ret])]
 
 
 --------------------------------------------------
@@ -89,8 +118,8 @@ evaluateTerm l = do
     let 
         deBru = lambdaToDeBru l []
         code  = (deBruToMacCode deBru)
-        result = evaluateWithPrint (code, [], [])
-    return result
+        result = evaluate' (code, [], [])
+    return (macCodeToString result)
 
 
 evaluate :: [MacCode a] -> (MacCode a)
@@ -120,13 +149,21 @@ codeStep (c:cs, env, stack) =
         (Acc n) -> (cs, env, [(env !! n)] ++ stack)
         (Ret)   -> (cs, fst (upackReturn stack), snd (upackReturn stack))
         (MacFunc f) -> (applyFunction f cs env stack)
+        (MBFunc f) -> (applyBoolFunc f cs env stack)
+        (MBool b) -> (cs, env, [MBool b] ++ stack)
+        (MacIf c1 c2) -> (applyIf c1 c2 cs env stack)
+
+-- Helper functions for codeStep
 
 applyFunction f cs env ((MacConst n):(MacConst m):stack) = (cs, env, (MacConst (f n m)):stack)
 
-upackReturn :: [(MacCode a)] -> ([MacCode a], [MacCode a])
+applyBoolFunc f cs env ((MacConst n):(MacConst m):stack) = (cs, env, (MBool (f n m)):stack)
+
+applyIf c1 c2 cs env ((MBool True):stack)  = (c1, env, [CloEnv cs env] ++ stack)
+applyIf c1 c2 cs env ((MBool False):stack) = (c2, env, [CloEnv cs env] ++ stack)
+
 upackReturn (v:(CloEnv e s):_) = (e,v:s)
 
-unpackApplication :: EvalState a -> EvalState a
 unpackApplication (cs, env, ((CloEnv cs' env'):v:rest)) = (cs', (v:env'), (CloEnv cs env):rest)
 
 macCodeToString :: Show a => MacCode a -> String
